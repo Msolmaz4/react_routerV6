0eb75ba9cd865c51ea3d154d6d6679788dfce04b
mustafa <msolmaz83@gmail.com>
2023-09-25 22:05:02 +0200

    active2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter ,Routes ,Route, Link, NavLink} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom'
 import Home from './pages/Home.tsx';
 import Contaxt from './pages/Contaxt.tsx';
 import Blog from './pages/Blog.tsx';
@@ -11,28 +11,30 @@ function App() {
 
     <BrowserRouter>
 
-    <nav>
-      {/* boyle ya[parsak sayfa yenileniyor ben  unu istemiyorum bunu yerine link ya[pariz]]
-       <a href='/'> Amasayda</a>
-      <a href='/contact'> Contact</a>
-      <a href='/blog'> Blog</a> */}
-{/* bunda ıse actıv klası eklıyemezsın o yuzden NAVLINK KULLAN
- <Link to='/'> Amasayda</Link>
-<Link to='/contact'> Contatct</Link>
-<Link to='/blog'>Blog</Link> */}
-     <NavLink to='/'> Amasayda</NavLink>
-<NavLink to='/contact'> Contatct</NavLink>
-<NavLink to='/blog'>Blog</NavLink>
+      <nav>
+        {/* boyle ya[parsak sayfa yenileniyor ben  unu istemiyorum bunu yerine link ya[pariz]]
+          <a href='/'> Amasayda</a>
+          <a href='/contact'> Contact</a>
+          <a href='/blog'> Blog</a> */}
+        {/* bunda ıse actıv klası eklıyemezsın o yuzden NAVLINK KULLAN
+           <Link to='/'> Amasayda</Link>
+           <Link to='/contact'> Contatct</Link>
+           <Link to='/blog'>Blog</Link> */}
+        <NavLink to='/' className={({isActive})=> isActive && 'aktif'}> Amasayda</NavLink>
+        <NavLink to='/contact' style={({isActive})=>({
+          backgroundColor: isActive ? 'black' :'transparent'
+        })}> Contatct</NavLink>
+        <NavLink to='/blog'>Blog</NavLink>
 
-    </nav>
-    
-     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/contact' element={<Contaxt/>}/>
-      <Route path='/blog' element={<Blog/>}/>
-     </Routes>
-    
-    
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contaxt />} />
+        <Route path='/blog' element={<Blog />} />
+      </Routes>
+
+
     </BrowserRouter>
   );
 }
